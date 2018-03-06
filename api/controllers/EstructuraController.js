4bd91456257f7b5f366368183a517b2b5448a6b5
Alejandro Marcano Van Grieken <alemvangrieken@gmail.com>
2018-03-06 08:04:40 -0400

    Initial commit

---
+++ api/controllers/EstructuraController.js
@@ -0,0 +1,11 @@
+/**
+ * EstructuraController
+ *
+ * @description :: Server-side logic for managing estructuras
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
